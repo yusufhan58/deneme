2abefca4b3c55c2144b62d32c3fa0b9ddb5b6575
yusufhan58 <yusuf_han_58@hotmail.com>
2021-04-13 10:04:49 +0300

    DeleteDuplicates.php is updated.

--- php/DeleteDuplicates.php
+++ php/DeleteDuplicates.php
@@ -1,68 +1,57 @@
 <?php
 
-class Node
+class ListNode
 {
-    public $data;
+    public $val;
     public $next;
 
     public function __construct()
     {
-        $this->data = 0;
+        $this->val = 0;
         $this->next = null;
     }
 }
 
-    function printL($x)
-    {
-        $temp = new Node();
-        $temp = $x;
-        if ($temp != null) {
-            while ($temp != null) {
-                echo $temp->data . " ";
+function deleteNode($temp)
+{
+    $a = new ListNode();
+    $init= new ListNode();
+    $init =$temp;
+    if ($temp != null) {
+        while ($temp != null && $temp->next != null) {
+            $a = $temp->next;
+            if ($temp->val == $a->val) {
+                $temp->next = $a->next;
+            } else {
                 $temp = $temp->next;
-            }
-        } else {
-            echo "The list is empty";
-        }
-        echo "<br>";
-    }
 
-    function deleteNode($temp)
-    {
-        $a = new Node();
-        $init= new Node();
-        $init =$temp;
-        if ($temp != null) {
-            while ($temp != null && $temp->next != null) {
-                $a = $temp->next;
-                if ($temp->data == $a->data) {
-                    $temp->next = $a->next;
-                    $temp=$temp->next;
-                } else {
-                    $temp = $temp->next;
-                }
             }
         }
-        return $init;
     }
+    return $init;
+}
 
 
 
-$first = new Node();
-$first->data = 1;
+$first = new ListNode();
+$first->val = 1;
 $first->next = null;
 
-/*$second = new Node();
-$second->data = 1;
+$second = new ListNode();
+$second->val = 1;
 $second->next = null;
-$first->next = $second;*/
+$first->next = $second;
 
-$third = new node();
-$third->data = 2;
+$third = new ListNode();
+$third->val = 1;
 $third->next = null;
-$first->next = $third;
+$second->next = $third;
 
+$fourth = new ListNode();
+$fourth->val = 1;
+$fourth->next = null;
+$third->next = $fourth;
+
+echo var_dump($first);
 $x=deleteNode($first);
-printL($x);
-
-
+echo var_dump($x);
